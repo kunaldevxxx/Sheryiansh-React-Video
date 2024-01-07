28adea4fa97d8cffd2d4ae40a0e764a55b002330
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-07 19:42:24 +0530

    Concept Of Routing And LINK And A Tag Diffrence

--- react-learn/Components/Header.js
+++ react-learn/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Header(props) {  //just pass props here and see page.js
   return (
@@ -6,9 +7,12 @@ function Header(props) {  //just pass props here and see page.js
         <h2 className='ml-2'>{props.user}</h2> 
 
         <div className='flex gap-4'>
-            <h4>{props.about}</h4>  //props driling use of prop of any other components
+            {/* <h4>{props.about}</h4>  //props driling use of prop of any other components
             <h4>{props.Contact}</h4>
-            <h4>{props.menu}</h4>
+            <h4>{props.menu}</h4> */}
+              <Link href="/About">ABOUT</Link>
+      <Link href="/Product">PRODUCT</Link>
+      <Link href="/Courses">COURSES</Link>
           
         </div>
     </div>
--- react-learn/app/page.js
+++ react-learn/app/page.js
@@ -2,12 +2,12 @@
 import Header from '@/Components/Header';
 import React  from 'react'
 import { useState } from 'react';
-
+import Link from 'next/link';
 const page = () => {
-  const [user,setuser] = useState("Kunal")
-  const [About,setAbout]=useState("About") //this is how we make it
-  const [ContactUs,setContactUs]=useState("Contact Us")
-  const [Menu,setMenu]=useState("Menu")
+  // const [user,setuser] = useState("Kunal")
+  // const [About,setAbout]=useState("About") //this is how we make it
+  // const [ContactUs,setContactUs]=useState("Contact Us")
+  // const [Menu,setMenu]=useState("Menu")
   return (
     <>
     {/* <h1 className='text-xl text-purple-500 font-extralight'>
@@ -16,8 +16,23 @@ const page = () => {
     <button onClick={()=>{
       setMarks(33)
     }} className='bg-purple-700 rounded-3xl mt-7 px-5 py-2 ml-5 text-white font-normal'>Update</button> */}
-    <Header user={user} about={About} Contact={ContactUs} menu={Menu}/> //pass here
-    {user}
+    {/* <Header user={user} about={About} Contact={ContactUs} menu={Menu}/> //pass here
+    {user} */}
+
+   
+    {/* <a href="/About"> About
+      </a>
+      <a href="/Product"> Product
+        </a>
+        <a href="Courses"> Courses
+          </a> */}  
+           {/* //a tag method its reloading so we use LINK/NEXT  */}
+    <Header/>
+    <h1>HomePage</h1>
+      
+
+     
+
     </>
   )
 }
