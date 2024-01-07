7045c8e614960e512fd0f4625fae5621ecec92e5
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-07 19:19:07 +0530

    Props Drilling And Example

--- react-learn/Components/Header.js
+++ react-learn/Components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Header(props) {
+function Header(props) {  //just pass props here and see page.js
   return (
     <div className='h-8 bg-green-500 flex gap-[83vw]'>
-        <h2 className='ml-2'>{props.user}</h2>
+        <h2 className='ml-2'>{props.user}</h2> 
 
         <div className='flex gap-4'>
-            <h4>About</h4>
-            <h4>About</h4>
-            <h4>About</h4>
-            <h4>About</h4>
+            <h4>{props.about}</h4>  //props driling use of prop of any other components
+            <h4>{props.Contact}</h4>
+            <h4>{props.menu}</h4>
+          
         </div>
     </div>
   )
--- react-learn/app/page.js
+++ react-learn/app/page.js
@@ -5,6 +5,9 @@ import { useState } from 'react';
 
 const page = () => {
   const [user,setuser] = useState("Kunal")
+  const [About,setAbout]=useState("About") //this is how we make it
+  const [ContactUs,setContactUs]=useState("Contact Us")
+  const [Menu,setMenu]=useState("Menu")
   return (
     <>
     {/* <h1 className='text-xl text-purple-500 font-extralight'>
@@ -13,7 +16,7 @@ const page = () => {
     <button onClick={()=>{
       setMarks(33)
     }} className='bg-purple-700 rounded-3xl mt-7 px-5 py-2 ml-5 text-white font-normal'>Update</button> */}
-    <Header user={user}/>
+    <Header user={user} about={About} Contact={ContactUs} menu={Menu}/> //pass here
     {user}
     </>
   )
